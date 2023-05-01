d1f2a2ab15f6973fba290e1bcaaa5b935ab586b0
André Lopes <andrellopes021@gmail.com>
2023-05-01 14:13:30 -0300

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './Styles/index.css';
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-violet-600 flex w-full h-full'>
+        <text>andre </text>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
